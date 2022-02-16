599058b32c5bf8b8626d59435edc5af821913ebe
zhou0123 <zhou812986561@gmail.com>
2022-02-16 22:56:22 +0800

    2.16

--- run_hj.sh
+++ run_hj.sh
@@ -1,6 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack single --test_mot15 True --method hijack
 CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack single --test_mot17 True --method hijack
-CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack single --test_mot20 True --method hijack
 CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack multiple --test_mot15 True --method hijack
 CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack multiple --test_mot17 True --method hijack
 CUDA_VISIBLE_DEVICES=0 python track.py --cfg ./cfg/yolov3_1088x608.cfg --weights /home/zhouchengyu/jde.uncertainty.pt --attack multiple --test_mot20 True --method hijack
